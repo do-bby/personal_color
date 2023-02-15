7cc03ea8905a7aeb31a7dc145702dc1fee849ca1
do-bby <qkrdbstn25@naver.com>
2023-02-15 15:17:59 +0900

    color 수정

--- personal/src/main/java/com/example/demo/entity/Color.java
+++ personal/src/main/java/com/example/demo/entity/Color.java
@@ -2,8 +2,12 @@ package com.example.demo.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -13,8 +17,17 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
+@SequenceGenerator(
+		  name = "COLOR_SEQ_GENERATOR",	
+		  sequenceName = "colorseq", 
+		  initialValue = 1,   			
+		  allocationSize = 1  
+)
 public class Color {
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="COLOR_SEQ_GENERATOR")
+	private int cnum;
 	@Column
 	private String hexcode;
 	
